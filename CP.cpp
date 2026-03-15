24b3a499cf2a01615ac145d784e199da9f74f814
PRASANJIT ROY <roy2402011@gmail.com>
2026-03-15 23:49:43 +0600

    by me

---
+++ CP.cpp
@@ -0,0 +1,32 @@
+/*************************************************************************
+*      Author : PRASANJIT ROY                                            *
+*      Date   : 27-02-2025                                               *
+*      Problem:  https://codeforces.com/problemset/problem/2205/G        *
+*      Notes:                                                            *
+*      - BFS / DFS / DP / Grid / Graph etc.                              *
+*************************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef double ld;
+
+void solve()
+{
+    
+}
+int main() {
+     int t;
+     cin>>t;
+     while(t--){
+        solve();
+     }
+    return 0;
+}
