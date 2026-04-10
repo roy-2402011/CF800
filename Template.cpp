03cfbf67622cded2e26fb520fe0d04f473840d04
PRASANJIT ROY <roy2402011@gmail.com>
2026-04-10 10:41:01 +0600

    by me

---
+++ Template.cpp
@@ -0,0 +1,40 @@
+
+/*******************************************************
+*                                                      *
+*    “Saraswati Mahabhage, Vidye Kamalalochane         *
+*    Vishwarupe Vishalakshi, Vidyam Dehi Namostute.”   *
+*                                                      *
+*******************************************************/
+
+
+//   "Every error is a step closer to the solution." 
+//    Author : Prasanjit Roy
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll   long long
+#define pb   push_back
+#define all(x) x.begin(), x.end()
+#define rep(i,a,b) for(int i=a; i<b; i++)
+
+const int INF  = 1e9;
+const ll  LINF = 1e18;
+const int MOD  = 1e9 + 7;
+
+void solve() {
+    
+     cout<<"PRASANJIT";
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    // cin >> t;  
+    while (t--) solve();
+
+    return 0;
+}
