cce72a3c8f02e7756bd4f40327b1e2b53fb8f089
PRASANJIT ROY <roy2402011@gmail.com>
2026-08-04 21:50:29 +0600

    by me

---
+++ A_Riptide.cpp
@@ -0,0 +1,40 @@
+/*******************************************************
+*                                                      *
+*    “Saraswati Mahabhage, Vidye Kamalalochane         *
+*    Vishwarupe Vishalakshi, Vidyam Dehi Namostute.”   *
+*                                                      *
+*******************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll   long long
+#define pb   push_back
+#define all(x) x.begin(), x.end()
+#define rep(i,a,b) for(int i=a; i<b; i++)
+
+const int INF  = 1e9;
+const ll  LINF = 1e18;
+const int MOD  = 1e9 + 7;
+
+void solve() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int d1 = abs(a - b);
+    int d2 = abs(b - c);
+    int d3 = abs(c - a);
+
+    cout << min(d1, min(d2, d3)) << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
